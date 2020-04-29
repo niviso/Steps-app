e5eeba115259876f4293b6676f0508ac423b215a
freenewipads2 <nikki.sollid@gmail.com>
2020-04-29 15:28:54 +0200

    you can now set time too!

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -7,23 +7,31 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 export default function popup(props){
   const {onSubmit} = props;
   const [text, setText] = React.useState('');
+  const [time,setTime] = useState(0);
+  const [showTime,setShowTime] = useState(false);
+  HandleTime = (e) =>{
+    setShowTime(false);
+    setTime(e.toString());
+  }
   return(
     <View style={styles.container}>
     <SimpleAnimation duration={500} staticType="zoom" style={styles.innerContainer}>
     <Text style={styles.headingText}>This is the heading text</Text>
     <TextInput
-      autoFocus
       style={styles.input}
       onChangeText={text => setText(text)}
       value={text}
       onSubmitEditing={() => onSubmit(text)}
     />
+    <TouchableOpacity onPress={() => setShowTime(true)}>
+    <Text>Set time: {time}</Text>
+    </TouchableOpacity>
     </SimpleAnimation>
     <DateTimePickerModal
-  isVisible={false}
-  mode="date"
-  onConfirm={null}
-  onCancel={null}
+  isVisible={showTime}
+  mode="time"
+  onConfirm={(e) => HandleTime(e)}
+  onCancel={setShowTime}
 />
     </View>
   )
